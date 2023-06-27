9d6ec80d6462dd80e76729829fc1a94f8800a2d0
Anand Rao <anandr1@anandr11MD6R.vmware.com>
2023-06-27 14:35:21 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import translate_apis
+class MyApp:
+    def __init__(self):
+        self.app = FastAPI()
+
+# Set up CORS middleware
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+
+# create an instance of the MyApp class
+app_instance = MyApp()
+app = app_instance.app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(translate_apis.router)
